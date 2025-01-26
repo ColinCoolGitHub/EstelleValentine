7dd3290246a59fae5c2549920b73030022baa300
ColinCoolGitHub <colingoulethardy@gmail.com>
2025-01-26 15:34:05 -0500

    book

--- bookjs.js
+++ bookjs.js
@@ -3351,20 +3351,3 @@ $.findPos = findPos;
 
 })(jQuery);
 
-$(window).ready(function() {
-    $('.pages').turn({
-      duration: 1500,
-      width: 620,
-      height: 445,
-    //  acceleration: true,
-    //  display: 'single',
-     // autoCenter: true,
-     turnCorners: "bl,br",
-      elevation: 300,
-      when: {
-        turned: function(e, page) {
-          console.log('Current view: ', $(this).turn('view'));
-        }
-      }
-    });
-  });
